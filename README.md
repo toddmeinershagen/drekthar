20c9ec751e3823178ed1f783bb2e5b35b25db19b
Todd Meinershagen <todd@meinershagen.net>
2025-06-05 18:22:12 -0500

    adding basic chat loop code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # drekthar
+
+## Initial Project Set Up
+
+1. [Set Up GitHub Repo](https://paycorinc-my.sharepoint.com/:w:/r/personal/tmeinershagen_paycor_com/Documents/how%20do%20i%20hook%20up%20a%20local%20git%20r.docx?d=wcc6b09271d674994a01d637f10b3c0dc&csf=1&web=1&e=QDMSG4)
+
+2. [Set Up Python Virtual Environment](https://paycorinc-my.sharepoint.com/:w:/r/personal/tmeinershagen_paycor_com/Documents/show%20me%20the%20commands%20needed%20to.docx?d=wf93b867bb7f24578b4a867b8f70c60ba&csf=1&web=1&e=sHoZ2Q)
+
+3. [Getting Started with Python Chat Loop](https://learn.microsoft.com/en-us/semantic-kernel/get-started/quick-start-guide?pivots=programming-language-python)
+
+4. [Referencing Class from Sub Folder](https://paycorinc-my.sharepoint.com/:w:/r/personal/tmeinershagen_paycor_com/Documents/how%20do%20i%20reference%20a%20python%20cl.docx?d=w8941f16ed53a4638a2723e20ded1a49f&csf=1&web=1&e=veMrtg)
---
+++ chat.py
@@ -0,0 +1,84 @@
+import asyncio
+import logging
+import os
+
+
+from dotenv import load_dotenv
+
+from semantic_kernel import Kernel
+from semantic_kernel.utils.logging import setup_logging
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
+from semantic_kernel.connectors.ai.chat_completion_client_base import ChatCompletionClientBase
+from semantic_kernel.contents.chat_history import ChatHistory
+from semantic_kernel.functions.kernel_arguments import KernelArguments
+
+from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.azure_chat_prompt_execution_settings import (
+    AzureChatPromptExecutionSettings,
+)
+
+from plugins.lights_plugin import LightsPlugin
+
+
+# Load the .env file
+load_dotenv()
+
+
+async def main():
+    # Initialize the kernel
+    kernel = Kernel()
+
+    # Add Azure OpenAI chat completion
+    chat_completion = AzureChatCompletion(
+        deployment_name = os.getenv("DEPLOYMENT_NAME"),
+        api_key=os.getenv("API_KEY"),
+        base_url=os.getenv("BASE_URL"),  # https://{resource}.openai.azure.com/openai/deployments/{deployment}/chat/completions?api-version={version}
+    )
+    kernel.add_service(chat_completion)
+
+    # Set the logging level for  semantic_kernel.kernel to DEBUG.
+    setup_logging()
+    logging.getLogger("kernel").setLevel(logging.DEBUG)
+
+    # Add a plugin (the LightsPlugin class is defined below)
+    kernel.add_plugin(
+        LightsPlugin(),
+        plugin_name="Lights",
+    )
+
+    # Enable planning
+    execution_settings = AzureChatPromptExecutionSettings()
+    execution_settings.function_choice_behavior = FunctionChoiceBehavior.Auto()
+
+    # Create a history of the conversation
+    history = ChatHistory()
+
+    # Initiate a back-and-forth chat
+    userInput = None
+    while True:
+        # Collect user input
+        userInput = input("User > ")
+
+        # Terminate the loop if the user says "exit"
+        if userInput == "exit":
+            break
+
+        # Add user input to the history
+        history.add_user_message(userInput)
+
+        # Get the response from the AI
+        result = await chat_completion.get_chat_message_content(
+            chat_history=history,
+            settings=execution_settings,
+            kernel=kernel,
+        )
+
+        # Print the results
+        print("Assistant > " + str(result))
+
+        # Add the message from the agent to the chat history
+        history.add_message(result)
+
+# Run the main function
+if __name__ == "__main__":
+    asyncio.run(main())
