d82c985ddf84237a845e17476e64ee5363ef635f
Todd Meinershagen <todd@meinershagen.net>
2025-06-10 00:32:37 -0500

    updating for rag search...still failing

--- chat.py
+++ chat.py
@@ -35,8 +35,6 @@ from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.azure_chat_
 from plugins.automation_plugin import AutomationPlugin
 from plugins.lights_plugin import LightsPlugin
 
-from typing import Awaitable, Callable
-from semantic_kernel.filters import FunctionInvocationContext
 from semantic_kernel.filters import FilterTypes, AutoFunctionInvocationContext
 
 
@@ -58,31 +56,6 @@ async def main():
     )
     az_source = AzureAISearchDataSource.from_azure_ai_search_settings(azure_ai_search_settings=search_settings)
     extra = ExtraBody(data_sources=[az_source])
-
-    # extra = {}
-    # data_sources = []
-    # extra["data_sources"] = data_sources
-    # data_source = {}
-    # data_source["type"] = "azure_search"
-    # parameters = data_source.setdefault("parameters", {})
-    # parameters["endpoint"] = "https://drekthar.search.windows.net"
-    # parameters["index_name"] = "drekthar"
-    # parameters["semantic_configuration"] = "default"
-    # parameters["query_type"] = "vector_semantic_hybrid"
-    # parameters["fields_mapping"] = {}
-    # # parameters["in_scope"] = True
-    # parameters["filter"] = None
-    # parameters["strictness"] = 3
-    # parameters["top_n_documents"] = 5
-    # authentication = parameters.setdefault("authentication", {})
-    # authentication["type"] = "api_key"
-    # authentication["key"] = os.getenv("SEARCH_ADMIN_KEY")
-    # # embedding = parameters.setdefault("embedding_dependency", {}) 
-    # # embedding["type"] = "deployment_name"
-    # # embedding["deployment_name"] = "text-embedding-3-small"
-    # data_sources.append(data_source)
-    
-    # settings = AzureChatPromptExecutionSettings()
     settings = AzureChatPromptExecutionSettings(extra_body=extra)
 
     chat_completion = AzureChatCompletion(
@@ -91,28 +64,9 @@ async def main():
         endpoint=HttpUrl(os.getenv("LLM_ENDPOINT")),
         api_version="2025-04-01-preview"
     )
-
-    # embedding = AzureTextEmbedding(
-    #     api_key=os.getenv("LLM_API_KEY"),
-    #     deployment_name="text-embedding-3-small",
-    #     endpoint=os.getenv("LLM_ENDPOINT")
-    # )
-
-    # memory_store = AzureCognitiveSearchMemoryStore(
-    #     vector_size=1536,
-    #     search_endpoint=os.getenv("SEARCH_ENDPOINT"),
-    #     admin_key=os.getenv("SEARCH_ADMIN_KEY")
-    # )
-
-    # memory_store.create_collection(collection_name="drekthar")
     
-    # memory = SemanticTextMemory(storage=memory_store, embeddings_generator=embedding)
-
     kernel = Kernel()
-    kernel.add_plugin(LightsPlugin(), plugin_name="lights")
-    kernel.add_plugin(AutomationPlugin(), plugin_name="automation")
-    # kernel.add_plugin(TextMemoryPlugin(memory), plugin_name="search")
-    
+
     # A filter is a piece of custom code that runs at certain points in the process
     # this sample has a filter that is called during Auto Function Invocation
     # this filter will be called for each function call in the response.
@@ -133,19 +87,7 @@ async def main():
         print(f"Number of function calls: {len(function_calls)}")
         # if we don't call next, it will skip this function, and go to the next one
         await next(context)
-        #############################
-        # Note: to simply return the unaltered function results, uncomment the `context.terminate = True` line and
-        # comment out the lines starting with `result = context.function_result` through `context.terminate = True`.
-        # context.terminate = True
-        #############################
-        result = context.function_result
-        if context.function.plugin_name == "math":
-            print("Altering the Math plugin")
-            context.function_result = FunctionResult(
-                function=result.function,
-                value="Stop trying to ask me to do math, I don't like it!",
-            )
-            context.terminate = True
+
 
     # Create the chat completion agent
     agent = ChatCompletionAgent(
@@ -155,7 +97,7 @@ async def main():
         instructions='''
         You are an AI assistant that helps people find information.
         ''',
-        # plugins=[ LightsPlugin(), AutomationPlugin(), TextMemoryPlugin(memory) ],
+        plugins=[ LightsPlugin(), AutomationPlugin() ],
         arguments=KernelArguments(settings=settings)
     )
     
